f65507434aa0b1168b2d55aaec3852c531892930
Wilkenson <tcbwilkenson@gamilcom>
2024-01-30 16:50:09 -0500

    prime

--- src/prime/Prime.java
+++ src/prime/Prime.java
@@ -1,25 +1,38 @@
 package prime;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class Prime {
     public static void main(String[] args) {
-        allPrime(10);
+        System.out.println(isPrime(5));
+
+        int[] numbers = new int[]{
+                2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+        };
+        allPrime(numbers);
     }
 
-    public static void allPrime(int n) { // method one
-        int[] numbers = new int[n];
-        List<Integer> prime = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            numbers[i] = i + 1;
+    public static boolean isPrime(double n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0 && n != 2)
+                return false;
         }
-
-        for (int nm : numbers){
-            System.out.print(nm);
-            System.out.print(" ");
-        }
+        return true;
     }
 
-    // another method
+    public static void allPrime(int[] num) {
+        List<Integer> prime = new ArrayList<>();
+        for (int n : num) {
+            if (isPrime(n)) {
+                prime.add(n);
+            }
+        }
+        System.out.println(prime);
+    }
+
 }
